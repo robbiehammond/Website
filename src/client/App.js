f43469e01a1db6ab46c5ab9fd587171609c836fa
Robbie Hammond <robbiehammond3@gmail.com>
2022-02-24 23:32:46 -0500

    Started adding pixi game linking

--- src/client/App.js
+++ src/client/App.js
@@ -1,5 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
   return (
@@ -8,11 +15,32 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           This will be the login page. You can log in or enter as guest.
+          Next step is to get the link to the pixi page working.
         </p>
-          
+        <div>
+        <Router>
+            <Routes>
+              <Route exact path="/" component={App} />
+              <Route path="/Game.html" component={<Game></Game>} />
+            </Routes>
+          </Router>
+        </div>
+
+    
       </header>
+      <div>
+
+      </div>
     </div>
   );
 }
+
+function Game() {
+  return (
+      <div>
+         <Link to="Game.html">Components</Link>
+      </div>
+   );
+  }
 
 export default App;
--- src/client/Login/Login.js
+++ src/client/Login/Login.js
@@ -10,7 +10,6 @@ class Login extends Component {
     }
 
     performSubmit(event) {
-        
             
     }
 
